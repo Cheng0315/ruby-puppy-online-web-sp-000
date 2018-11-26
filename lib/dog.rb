b54934cbccfbd9f1742468889efbb4fa76150bca
Cheng Vue <cvu3@ucsc.edu>
2018-11-26 11:15:12 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,13 @@
 class Dog
   attr_accessor :name
   @@all = []
-  @@all_name = []
   def initialize(name)
     @name = name
     @@all << self
-    @@all_name << name
   end
 
   def self.all
-    @@all_name.each do |name|
+    @@all.each do |name|
       puts name
     end
   end
